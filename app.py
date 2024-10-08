de2a442c17886d64a35d76a10786e0bc5b424f2a
cypri12 <167737781+cypri12@users.noreply.github.com>
2024-10-08 18:18:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/run-script')
+def run_script():
+    # Exécuter le script Python
+    result = subprocess.run(['python3', 'text.py'], capture_output=True, text=True)
+    return f"<pre>{result.stdout}</pre>"
 
 if __name__ == '__main__':
     app.run(debug=True)
